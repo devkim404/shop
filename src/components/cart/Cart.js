948dc91da9feb1b94a90cc1d7dddc06c41fee3c6
devkim404 <120579845+devkim404@users.noreply.github.com>
2023-01-08 19:40:07 +0900

    장바구니 수량변경

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,24 +1,49 @@
 import { Table } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import { increase, addCount } from '../../hooks/store';
 
 export default function Cart() {
+
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const cart = state.cart;
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>상품명</th>
-          <th>수량</th>
-          <th>변경하기</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>안녕</td>
-          <td>안녕</td>
-          <td>안녕</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <h2>
+        {state.user.name}{state.user.age}의 장바구니
+      </h2>
+      <Button variant="secondary" onClick={() => {
+        dispatch(increase());
+      }}>+</Button>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart &&
+            cart.map((product, productIdx) => (
+              <tr key={cart[productIdx].id}>
+                <td>{cart[productIdx].id}</td>
+                <td>{cart[productIdx].name}</td>
+                <td>{cart[productIdx].count}</td>
+                <td>
+                  <Button variant="secondary" onClick={() => {
+                    dispatch(addCount(cart[productIdx].id));
+                  }}>+</Button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+    </>
   )
 }
--- src/hooks/store.js
+++ src/hooks/store.js
@@ -1,7 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const user = createSlice({
+  name: 'user',
+  initialState : {
+    name : 'kim',
+    age : 20,
+  },
+  reducers: {
+    // changeName(state) {
+    //   state.name = 'park';
+    // },
+    increase(state) {
+      state.age += 1;
+    },
+  }
+})
+
+const cart = createSlice({
+  name: 'cart',
+  initialState: [
+    {id : 0, name : 'White and Black', count : 2},
+    {id : 2, name : 'Grey Yordan', count : 1},
+  ],
+  reducers: {
+    addCount(state, action) {
+      const idx = state.findIndex((e) => e.id === action.payload);
+      state[idx].count++;
+    }
+  }
+})
 
 export default configureStore({
   reducer: {
-    
+    user: user.reducer,
+    cart: cart.reducer,
   }
-}) 
+})
+
+export const { increase } = user.actions;
+export const { addCount } = cart.actions;
